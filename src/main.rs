5e89fb1ae25640ee810c3eefc5ac5203f72d6498
Leigh Johnson <hi@leighjohnson.me>
2021-07-11 14:17:22 -0700

    rename library to printnanny; move find_matches fn to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,6 @@ use log::{info, warn};
 use structopt::StructOpt;
 extern crate clap_verbosity_flag;
 
-fn answer() -> i32 {
-    42
-}
-
-
-#[test]
-fn check_answer_validity(){
-    assert_eq!(answer(), 42)
-}
-
 #[derive(StructOpt)]
 struct Cli {
     pattern: String,
@@ -24,21 +14,6 @@ struct Cli {
     // flags: -v (warnings) -vv (info) -vvv(debug) and -vvvv (trace)
     #[structopt(flatten)]
     verbose: clap_verbosity_flag::Verbosity
-}
-
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
-    for line in content.lines() {
-        if line.contains(pattern){
-            writeln!(writer, "{}", line);
-        }
-    }  
-}
-
-#[test]
-fn find_a_match(){
-    let mut result: Vec<u8> = Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem ipsum\n")
 }
 
 fn main() -> Result<()> {
@@ -60,7 +35,8 @@ fn main() -> Result<()> {
     let path = "test.txt";
     let content = std::fs::read_to_string(path)
         .with_context(|| format!("could not read file `{}`", path))?;
-    
-    // find_matches();
+    let mut result: Vec<u8> = Vec::new();
+
+    printnanny::find_matches(&content, &args.pattern, result);
     Ok(())
 }
