ce01a011d4b5c7c22f9a8906d3d3108c424ac2a2
Leigh Johnson <hi@leighjohnson.me>
2021-07-11 19:01:56 -0700

    preparing to implement print-nanny-auth

--- tests/cli.rs
+++ tests/cli.rs
@@ -4,29 +4,29 @@ use assert_cmd::Command;
 use tempfile::NamedTempFile;
 use std::io::Write;
 
-#[cfg(target_arch="x86_64")]
-#[test]
-fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("printnanny")?;
+// #[cfg(target_arch="x86_64")]
+// #[test]
+// fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+//     let mut cmd = Command::cargo_bin("printnanny")?;
 
-    cmd.arg("foobar").arg("test/file/doesnt/exist");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("No such file or directory"));
-    Ok(())
-}
+//     cmd.arg("foobar").arg("test/file/doesnt/exist");
+//     cmd.assert()
+//         .failure()
+//         .stderr(predicate::str::contains("No such file or directory"));
+//     Ok(())
+// }
 
-#[cfg(target_arch="x86_64")]
-#[test]
-fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = NamedTempFile::new()?;
-    writeln!(file, "A test\nMore content\nLorem ipsum\nAnother test")?;
+// #[cfg(target_arch="x86_64")]
+// #[test]
+// fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+//     let mut file = NamedTempFile::new()?;
+//     writeln!(file, "A test\nMore content\nLorem ipsum\nAnother test")?;
 
-    let mut cmd = Command::cargo_bin("printnanny")?;
-    cmd.arg("test").arg(&file.path());
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::contains("test\nAnother test"));
-    Ok(())
+//     let mut cmd = Command::cargo_bin("printnanny")?;
+//     cmd.arg("test").arg(&file.path());
+//     cmd.assert()
+//         .success()
+//         .stdout(predicate::str::contains("test\nAnother test"));
+//     Ok(())
 
-}
+// }
