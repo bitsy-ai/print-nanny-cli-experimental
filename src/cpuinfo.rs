cfba9f4ca5377de21b0b445ad6465461a1d059bb
Leigh Johnson <hi@leighjohnson.me>
2021-07-16 20:00:20 -0700

    add cpuinfo module, tests, fixtures

---
+++ src/cpuinfo.rs
@@ -0,0 +1,45 @@
+use std::fs;
+use std::collections::{ HashMap };
+use anyhow::{ Context, Result };
+
+#[derive(Debug, Clone)]
+pub struct CpuInfo {
+   pub processors: Vec<HashMap<String, String>>, // Vec of parsed processor blocks
+   pub extra: Option<HashMap<String, String>> // common fields
+}
+
+impl CpuInfo {
+
+    pub fn from_string(data: &str) -> CpuInfo {
+        let vec_map = CpuInfo::parse_blocks(&data);
+        let processors = vec_map
+            .iter()
+            .filter(|x| x.contains_key("processor"))
+            .cloned()
+            .collect();
+        let extra = vec_map.iter().filter(|x| !x.contains_key("processor")).next().cloned();
+        CpuInfo {
+            processors, extra
+        }
+    }
+    pub fn new() -> Result<CpuInfo> {
+        let filename = "/proc/cpuinfo";
+        let data = fs::read_to_string(filename)?;
+        Ok(CpuInfo::from_string(&data))
+    }
+
+    fn parse_line(line: &str) -> (String, String) {
+        let errmsg = format!("Failed to parse line {}", line.to_string());
+        let splitpoint = line.find(":").expect(&errmsg);
+        let (key, value) = line.split_at(splitpoint);
+        ( key.trim().to_string(), value[1..].trim().to_string() )
+    }
+
+    fn parse_blocks(data: &str) -> Vec<HashMap<String, String>> {
+        data.split("\n\n")
+            .map(|block| 
+                block.trim()
+                    .split("\n")
+                    .map(CpuInfo::parse_line).collect()).collect()
+    }
+}
---
+++ tests/cpuinfo.rs
@@ -0,0 +1,37 @@
+use std::fs;
+use anyhow::{ Context, Result };
+
+use printnanny::cpuinfo::{ CpuInfo };
+
+
+#[test]
+fn test_rpi_cpuinfo() -> Result<()> {
+    let data = fs::read_to_string("tests/fixtures/rpi_cpuinfo.txt")?;
+    let cpuinfo = CpuInfo::from_string(&data);
+
+    println!("Parsed cpuinfo {:#?}", cpuinfo);
+    assert!(cpuinfo.processors.len() == 4);
+
+    // let expected_model = "Raspberry Pi 4 Model B Rev 1.1";
+    match cpuinfo.extra {
+        Some(x) => {
+            assert!(x["Hardware"] == "BCM2711");
+            assert!(x["Model"] == "Raspberry Pi 4 Model B Rev 1.1");
+            assert!(x["Revision"] == "c03111");
+            assert!(x["Serial"] == "100000003fa9a39b");
+        },
+        None => {}
+    };
+    Ok(())
+}
+
+#[test]
+fn test_amd64() -> Result<()> {
+    let data = fs::read_to_string("tests/fixtures/amd64_cpuinfo.txt")?;
+    let cpuinfo = CpuInfo::from_string(&data);
+
+    println!("Parsed cpuinfo {:#?}", cpuinfo);
+    assert!(cpuinfo.processors.len() == 64);
+    assert!(cpuinfo.extra == None );
+    Ok(())
+}
